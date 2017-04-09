8c4b6464ae8195d67200bf8fdff2029ba91a68c2
Mats Roshauw <karmats@gmail.com>
2017-04-09 13:00:13 +0200

    Version upgrades with corresponding fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -45,6 +46,7 @@ import { ProjectComponent, ProjectDialog } from './project/project.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     FlexLayoutModule,
